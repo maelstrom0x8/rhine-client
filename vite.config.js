406aeb2c09597e7a993dfe47b6b9c2745a27094b
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-28 23:42:39 +0100

    Initial commit Project setup and configurations

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const path = require('path')
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8001,
+    strictPort: true,
+    open: true,
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080'
+      },
+      '/user': {
+        target: 'http://localhost:8080'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "~": path.resolve('src')
+    }
+  }
+
+})
