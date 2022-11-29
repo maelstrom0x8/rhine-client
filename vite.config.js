8f98aa1f3e40127231f92d1df22c20456dd02dc2
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-11-29 09:22:10 +0100

    fix: module imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const path = require('path')
+import * as path from 'path'
+
 
 export default defineConfig({
   plugins: [react()],
