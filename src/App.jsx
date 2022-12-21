eaff56f7e8885e97b58fdc1950defcc15d295c1b
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-12-21 18:37:54 +0100

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import Header from "components/Header";
 
 const App = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <div
+      className="min-h-screen bg-neutral-300 text-black dark:bg-black dark:text-white 
+    flex-col flex p-1 space-y-2 space-y-reverse"
+    >
+      <Header/>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="flex max-w-full h-auto mx-1 ">
+      {/* logo */}
+      <h4 className="max-sm:text-2xl p-6">RhineTasks</h4>
+    </div>
+  );
+};
