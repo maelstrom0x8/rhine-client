3c64579b7e3088bc274c967a6754f338a0572256
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-12-29 02:46:07 +0100

    Update active list to last added

--- src/context/tasks/Context.jsx
+++ src/context/tasks/Context.jsx
@@ -1,53 +1,58 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const API_URL_PATH = "/api/v1/rhine";
 
 export const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
-  
+
   const [list, setList] = useState([]);
   const [tasks, setTasks] = useState([]);
 
   const [activeListID, setActiveListID] = useState(1);
 
-  const getListTasks = (listID = 1) => {
+  const getListTasks = (listID) => {
     axios
       .get(`${API_URL_PATH}/task`, { params: { list_id: `${listID}` } })
       .then((res) => setTasks(res.data))
-      .catch((err) => {});
+      .catch((err) => { });
     return tasks;
   };
 
   const addList = (title) => {
     axios
       .post(`${API_URL_PATH}/list/create`, null, { params: { name: `${title}` } })
+      .then(res => { setList([...list, res.data]) })
       .catch((err) => {
         console.error(err);
       });
-    updateList();
   };
 
   const addTask = (values) => {
-    axios.post(`${API_URL_PATH}/task/create`, {...values}, {params: {list_id: `${activeListID}`}})
+    axios.post(`${API_URL_PATH}/task/create`, { ...values }, { params: { list_id: `${activeListID}` } })
   }
 
   const updateList = () => {
     axios
       .get(`${API_URL_PATH}/list`)
       .then((res) => setList(res.data))
-      .catch((err) => {});
+      .catch((err) => { });
   };
 
   useEffect(() => {
+    const [last] = [...list].reverse();
+    if (last && last.id) {
+      setActiveListID(last.id)
+    }
+  }, [list])
+
+  useEffect(() => {
     updateList();
-  });
+  }, []);
 
   return (
-    <TaskContext.Provider value={{ list, getListTasks, addList, addTask , activeListID, setActiveListID}}>
+    <TaskContext.Provider value={{ list, getListTasks, addList, addTask, activeListID, setActiveListID }}>
       {children}
     </TaskContext.Provider>
   );
