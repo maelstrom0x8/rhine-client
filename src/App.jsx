a97daaf29896917decf40097a5c381731ac8ea6a
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-16 03:24:47 +0100

    add: routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import TaskModal from "~/components/Modal/TaskModal";
 import ListModal from "~/components/Modal/ListModal";
 import { TaskProvider } from "~/context/tasks/Context";
 import ListContent from "~/components/TaskPanel/ListContent";
+import UserModal from "~/components/Modal/UserModal";
+import { Route, Routes } from "react-router-dom";
 
 
 
@@ -17,6 +19,7 @@ import ListContent from "~/components/TaskPanel/ListContent";
 const App = () => {
   const [showListModal, setShowListModal] = useState(false);
   const [showTaskModal, setShowTaskModal] = useState(false);
+  const [showUserModal, setShowUserModal] = useState(false)
 
   const handleTaskClose = () => {
     setShowTaskModal(false);
@@ -26,19 +29,38 @@ const App = () => {
     setShowListModal(false)
   }
 
+  const handleBlur = ev => {
+    setShowUserModal(false)
+    console.log("Lost Focus");
+  }
+
+
   return (
     <div
       className="min-h-screen bg-neutral-300 text-black dark:bg-neutral-800 dark:text-neutral-300
     flex-col flex p-1 space-y-2 space-y-reverse transition-colors delay-500 duration-500 ease-in-out"
     >
+
+      <Routes>
+        <Route path="/" element={<></>} />
+        <Route path="/profile" />
+        <Route path="/privacy-policy"/>
+        <Route path="terms-of-service"/>
+        <Route path="/oss-license"/>
+        <Route path="/settings"/>
+        <Route path="/register"/>
+        <Route path="/login"/>
+      </Routes>
+
+
       <div
         className="bg-blue-700 dark:bg-teal-600/95 transition-colors delay-500 duration-500 ease-in-out p-4 max-w-md mx-auto fixed bottom-[3em] items-center
           right-[3em] z-0 rounded-full w-[4em] h-[4em] hover:bg-blue-500 shadow-md hover:shadow-neutral-700"
         onClick={() => setShowTaskModal(true)}
       ><FontAwesomeIcon icon={faPencil} color="white" className="w-7 h-7 ml-[3px] mt-1" /></div>
 
-      <Navbar />
-
+      <Navbar openUserModal={() => {setShowUserModal(!showUserModal)}} />
+      <UserModal isOpen={showUserModal} onBlur={handleBlur} className={"fixed top-0 bottom-0 left-0 right-0"} />
       <TaskProvider>
 
         <ListModal isOpen={showListModal} onClose={handleListModalClose} />
