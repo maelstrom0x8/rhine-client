bf15a1e467e2260934e46d442ada57680289e503
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-01-16 03:26:20 +0100

    feat: user icon on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useLayoutEffect } from "react";
 
 
-export const Navbar = () => {
+export const Navbar = ({openUserModal = f => f}) => {
 
   const handleTheme = () => {
     if (localStorage.theme !== "dark" || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -15,21 +15,23 @@ export const Navbar = () => {
     }
   }
 
-  useLayoutEffect(() => { handleTheme() }, [])
+  // useLayoutEffect(() => { handleTheme() }, [])
 
   return (
     <div className="flex flex-row max-w-full mx-1 items-center py-4 sm:px-4 pl-2 min-lg:px-10">
       <h1 className="max-sm:text-2xl max-lg:text-xl">RhineTasks</h1>
       <div className="ml-auto mr-4">
         <div className="inline-flex items-center space-x-5 lg:space-x-5">
-          <div>
+        <div className="focus:outline focus:outline-slate-50 focus:outline-2 w-5 h-5 rounded-full">
             <FontAwesomeIcon
               icon={faCircleHalfStroke}
               className="sm:w-6 sm:h-6 max-lg:w-5 max-lg:h-5 hover:shadow-lg hover:cursor-pointer"
               onClick={handleTheme} />
           </div>
-          <div>
-            <FontAwesomeIcon icon={faCircleUser} className="sm:w-6 sm:h-6 max-lg:w-5 max-lg:h-5 hover:shadow-lg hover:cursor-pointer" />
+          <div className="focus:outline focus:outline-slate-50 focus:outline-2 w-5 h-5 rounded-full"
+            onClick={openUserModal}
+          >
+            <FontAwesomeIcon icon={faCircleUser} className="sm:w-6 sm:h-6 max-lg:w-5 max-lg:h-5 hover:shadow-lg hover:cursor-pointer " />
 
           </div>
         </div>
