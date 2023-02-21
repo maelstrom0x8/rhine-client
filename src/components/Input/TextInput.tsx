61b8161f8771f754270a5e1d18f2ccb95038fff7
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-21 09:46:39 +0100

    add: custom text input component

--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -1,19 +1,25 @@
+import React from 'react';
 
 type InputProps = {
-    type : 'text' | 'password'
-    placeholder: string
-    required: boolean
-    handleChange: () => void
-}
+  type: 'text' | 'password';
+  placeholder: string;
+  required: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-export const TextInput = (props : InputProps) => {
-    return (
-        <input
-            className="focus:border-l-4 focus:border-l-blue-300 focus:font-semibold  pl-1 shadow-sm w-full text-black text-sm"
-            type={props.type}
-            placeholder={props.placeholder}
-            required={props.required}
-            onChange={props.handleChange}
-          />
-    )
-}
+export const TextInput = ({
+  type,
+  placeholder,
+  required,
+  onChange,
+}: InputProps) => {
+  return (
+    <input
+      className="focus:border-l-4 focus:border-l-blue-300 focus:font-semibold  pl-1 shadow-sm w-full text-black text-sm"
+      type={type}
+      placeholder={placeholder}
+      required={required}
+      onChange={onChange}
+    />
+  );
+};
