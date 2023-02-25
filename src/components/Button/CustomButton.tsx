71ff4aa515e083856f873eb52e0ee821faaaf6fb
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-02-25 03:12:59 +0100

    extend button props

--- src/components/Button/CustomButton.tsx
+++ src/components/Button/CustomButton.tsx
@@ -1,10 +1,15 @@
-import { ButtonProps } from './ButtonProps';
 
-const CustomButton = (props: ButtonProps) => {
+type CustomButtonProp = {
+  className?: string
+  text?: string;
+  onClick?: () => void;
+};
+
+const CustomButton = (props: CustomButtonProp) => {
   return (
     <button
-      className="max-w-sm shrink-0 px-2 items-center bg-neutral-900 dark:bg-neutral-200/80 dark:text-black text-neutral-50 hover:border-neutral-600 hover:cursor-pointer inline-flex space-x-0 max-sm:text-sm mr-auto p-4"
-      onClick={props.handleClick}
+      className={`${props.className} max-w-sm shrink-0 px-2 items-center bg-neutral-900 dark:bg-neutral-200/80 dark:text-black text-neutral-50 hover:border-neutral-600 hover:cursor-pointer inline-flex space-x-0 max-sm:text-sm mr-auto p-4`}
+      onClick={props.onClick}
     >
       {props.text}
     </button>
